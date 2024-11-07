fd41080ece50183b4714e134da3d25b7575166d5
Ketan Ghungralekar <Ketan.Ghungralekar@iiitb.ac.in>
2024-11-08 00:39:20 +0530

    Added Backend

---
+++ Language_Learning_Backend/src/main/java/com/example/Language_Learning_App/LanguageLearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Language_Learning_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageLearningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageLearningAppApplication.class, args);
+	}
+
+}
---
+++ Language_Learning_Backend/src/test/java/com/example/Language_Learning_App/LanguageLearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Language_Learning_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageLearningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
