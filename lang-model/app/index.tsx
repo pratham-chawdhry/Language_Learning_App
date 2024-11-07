cf533a6c19d1d19bc8b186aa1d634fc7d95eaf1a
pratham-chawdhry <122348596+pratham-chawdhry@users.noreply.github.com>
2024-11-07 17:56:41 +0530

    Second commit

---
+++ lang-model/app/index.tsx
@@ -0,0 +1,31 @@
+import { Image, StyleSheet, Platform, Text, View, ScrollView} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Link, Redirect, router} from 'expo-router';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import "../global.css";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className='bg-primary h-full' >
+      <ScrollView contentContainerStyle = {{height: "100%"}} >
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a limitless exploration with Aora</Text>
+        </View>
+      </ScrollView>
+      <StatusBar style="light" backgroundColor='#161622'/>
+    </SafeAreaView>
+  );
+}
+
